fc54d91e972d153f57cb280ed77e3fccec88157a
Fernando Castro <fernandocastroguiter@gmail.com>
2025-02-20 21:49:07 -0300

    Exercico modelo de OO em java

---
+++ src/Carro.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+
+public class Carro {
+    // variaveis
+    // tipo de dados primitivo
+    public int ano;
+    public float velocidade;
+    // tipo de dado é uma classe
+    public String marca, modelo;
+
+    // metodos construtores
+    // em parâmetros
+    public Carro(){
+        this.marca = "Vazia";
+        this.modelo = "Vazia";
+    }
+
+    public Carro(int ano, float velocidade, String marca, String modelo){
+        this.ano = ano;
+        this.velocidade = velocidade;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    public void exibirDetalhes(){
+        //this representa o objeto que chama o metodo
+        JOptionPane.showMessageDialog(null,"Marca: " + this.marca +
+                " | Modelo: " + this.modelo +
+                " | Ano: " + this.ano +
+                " | Velocidade: " + this.velocidade);
+    }
+
+    public void acelerar(float x){
+        this.velocidade += x;
+    }
+    public void frear(float x){
+        if(this.velocidade - x >= 0){
+            this.velocidade -= x;
+        }
+        else{
+            System.out.println("Freagem não ocorreu");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // criando objetos da classe Carro
+        // instanciar a classe Carro
+        Carro obj1 = new Carro(2015, 0.0f, "Chevrolet", "Onix");
+        obj1.exibirDetalhes();
+        obj1.acelerar( 60);
+        obj1.exibirDetalhes();
+        obj1.frear(70);
+        obj1.exibirDetalhes();
+
+
+        Carro obj2 = new Carro(2010, 30.0f, "Volks", "Gol");
+        obj2.exibirDetalhes();
+        obj2.acelerar(50);
+        obj2.exibirDetalhes();
+        obj2.frear(40);
+        obj2.exibirDetalhes();
+
+        Carro obj3 = new Carro();
+        obj3.exibirDetalhes();
+    }
+}
