0db50d7787c93abd5908bc3433a3badfef2c948a
Fernando Castro <fernandocastroguiter@gmail.com>
2025-02-27 21:17:34 -0300

    Exe 1 Alunos

---
+++ Lista1/src/MainAluno.java
@@ -0,0 +1,14 @@
+import exe1.Aluno;
+public class MainAluno {
+    public static void main(String[] args) {
+        // instancia a classe Aluno -> objeto
+        Aluno obj1 = new Aluno(); // chama construtor
+        System.out.println("Nome " + obj1.nome + " Média " + obj1.notaFinal());
+        obj1.dadosAluno();
+        Aluno obj2= new Aluno(123, "Tiago", 19, 5.4f, 8.9f);
+        System.out.println("Nome " + obj2.nome + " Média " + obj2.notaFinal());
+        obj2.dadosAluno();
+
+
+    }
+}
